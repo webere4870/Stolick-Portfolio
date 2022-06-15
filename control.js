d47b3355698e1d249f783570f7bb9bd40a9ff692
Eli Weber <eliweber2001@gmail.com>
2022-06-15 17:08:23 -0400

    Slideshow added

--- control.js
+++ control.js
@@ -23,7 +23,15 @@ $().ready(()=>
 
     $(".iframe").click((evt)=>
     {
-        $(`.${evt.target.id}`).toggle(400)
+        $('.xBtn').toggle(400)
+        $("." + evt.target.id).toggle(400)
+    })
+
+
+    $('.xBtn').click((evt)=>
+    {
+        $('.xBtn').toggle(400)
+        $('.courses').toggle(400)
     })
 })
 
@@ -45,3 +53,32 @@ marker.on("click", function (event) {
     event.target.openPopup();
     });
 mymap.invalidateSize();
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
