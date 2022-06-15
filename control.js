2bbf24d5b745d3ee3649cd25aacd3ce6833dbc70
Eli Weber <eliweber2001@gmail.com>
2022-06-15 17:56:06 -0400

    Ethics bowl page started

--- control.js
+++ control.js
@@ -71,14 +71,17 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
+  let quotes = document.getElementsByClassName("ethics")
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    quotes[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  quotes[slideIndex-1].style.display = "flex";
 }
